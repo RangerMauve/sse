1beb60ca515c53a036be7f23171fceb5cd9b2b77
RangerMauve <RangerMauve@hotmail.com>
2014-06-24 13:31:41 -0400

    1.0.0

---
+++ index.js
@@ -0,0 +1,49 @@
+/*
+ * SSE
+ *
+ * Copyright (c) 2014 RangerMauve
+ * Licensed under the MIT license
+ */
+var EventEmitter = require("eventemitter2");
+
+function SSE(url, options) {
+	var self = this;
+	var sse = new EventSource(url);
+	this.connection = sse
+	EventEmitter.call(this);
+	this.has_registered = {};
+	sse.onerror = function (e) {
+		self.emit("error", e);
+	}
+	sse.onmessage = function (e) {
+		self._emit_sse("message", e);
+	}
+}
+
+SSE.prototype = Object.create(EventEmitter.prototype);
+
+SSE.prototype._listen_to_sse = function (event) {
+	if (event in this.has_registered) return;
+	var self = this;
+	this.has_registered[event] = true;
+	this.connection.addEventListener(event, function (e) {
+		self._emit_sse(event, e);
+	});
+}
+
+SSE.prototype._emit_sse = function (event, e) {
+	var data = e.data;
+	try {
+		var parsed = JSON.parse(data);
+		self.emit(event, parsed);
+	} catch (e) {
+		self.emit(event, data);
+	}
+}
+
+SSE.prototype.on = function (event) {
+	this._listen_to_sse(event);
+	EventEmitter.prototype.on.apply(this, arguments);
+}
+
+module.exports = SSE;
